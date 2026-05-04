175ba9aaded2598d33086dcb0af399c1ee20c013
Ousmanyaouba <yaoubaousman@gmail.com>
2026-05-05 02:41:23 +0300

    Force CORS headers

---
+++ backend/cors_fix2.py
@@ -0,0 +1,33 @@
+with open('main.py', 'r') as f:
+    content = f.read()
+
+old = '''app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)'''
+
+new = '''@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)'''
+
+content = content.replace(old, new)
+
+with open('main.py', 'w') as f:
+    f.write(content)
+
+print("Done")
--- backend/main.py
+++ backend/main.py
@@ -18,11 +18,19 @@ models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=False,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
